35e5737ca677c8553fb89a88e391c2ade1d8e63d
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-01-06 14:07:49 +0530

    Update sketch.py

--- handlers/sketch.py
+++ handlers/sketch.py
@@ -4,11 +4,12 @@ from pyrogram.types import Message
 from handlers.help import *
 from PIL import Image
 
-user = message.from_user.id
+
 
 @Client.on_message(filters.command("sketch", ["-"]) & filters.me)
 async def progress(client: Client, message: Message):
     x = await message.edit("<Strong>Making Sketch...</Sketch>")
+    user = message.from_user.id
     image = await Image.open(user)
     image = await image.convert("L")
     image = await Image.eval(image, lambda x: 255-x)
