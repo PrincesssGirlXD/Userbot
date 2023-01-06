b7ebc6ba730f0ab304f18ee22e0aedf6f88a1c21
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-01-06 14:04:27 +0530

    Update sketch.py

--- handlers/sketch.py
+++ handlers/sketch.py
@@ -8,7 +8,6 @@ from PIL import Image
 @Client.on_message(filters.command("sketch", ["-"]) & filters.me)
 async def progress(client: Client, message: Message):
     x = await message.edit("<Strong>Making Sketch...</Sketch>")
-    image_file = await Client.download_media(message, progress=progress)
     image = await Image.open(image_file)
     image = await image.convert("L")
     image = await Image.eval(image, lambda x: 255-x)
